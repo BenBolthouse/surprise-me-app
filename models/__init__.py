4faf1cc0d345892b8acafd2f0b033e20b976ef8c
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-03 06:29:22 -0700

    Add new message model

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
 from .user_connection import UserConnection
+from .message import Message
---
+++ models/message.py
@@ -0,0 +1,20 @@
+from .db import db
+
+
+class Message(db.Model, UserMixin):
+    __tablename__ = "messages"
+
+    # Properties
+    id = db.Column(db.Integer, primary_key=True)
+    user_connection_id(db.Integer,
+                       db.ForeignKey('user_connections.id'),
+                       nullable=False)
+    sender_user_id = db.Column(db.Integer, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    deleted = db.Column(db.Boolean, nullable=False, default=False)
+    updated = db.Column(db.Boolean, nullable=False, default=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    # TODO implement association getters and setters
+
+    # TODO implement scopes while creating routes
