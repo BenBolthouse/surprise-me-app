ec90de35c33ade2f030732fa13f882ada00533d2
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-02 19:03:01 -0700

    Add more configuration to flask app and connect database

--- flask_api/__init__.py
+++ flask_api/__init__.py
@@ -1,3 +1,34 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+from .config import Config
+from .models import db
+
 
 app = Flask(__name__)
+
+# Environment setup
+app.config.from_object(Config)
+
+# User login configuration
+login = LoginManager(app)
+login.login_view = 'auth.unauthorized'
+
+@login.user_loader
+def load_session_user(id):
+    # TODO add scoped user object to session
+    pass
+
+
+# Route configuration
+# TODO add routes to config
+
+# Models and migration configuration
+db.init_app(app)
+Migrate(app, db)
+
+# Security configuration
+CORS(app)
