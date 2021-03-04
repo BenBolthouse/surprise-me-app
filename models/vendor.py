e70569af5cea580f85e62728afda5744fe48c39a
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-03 19:47:18 -0700

    Fix readability of Vendor model

--- models/vendor.py
+++ models/vendor.py
@@ -8,15 +8,24 @@ class Vendor(db.Model):
     __tablename__ = "vendors"
 
     # Properties
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable=False)
-    coord_lat = db.Column(sa.types.DECIMAL(precision=9, scale=6), nullable=False)
-    coord_long = db.Column(sa.types.DECIMAL(precision=8, scale=6), nullable=False)
+    id = db.Column(
+        db.Integer,
+        primary_key=True)
+    name = db.Column(
+        db.String(64),
+        nullable=False)
+    coord_lat = db.Column(
+        sa.types.DECIMAL(precision=9, scale=6),
+        nullable=False)
+    coord_long = db.Column(
+        sa.types.DECIMAL(precision=8, scale=6),
+        nullable=False)
 
     # Associations
-    _products = db.relationship("VendorProduct",
-                                back_populates="_vendor",
-                                cascade="all, delete-orphan")
+    _products = db.relationship(
+        "VendorProduct",
+        back_populates="_vendor",
+        cascade="all, delete-orphan")
 
     # TODO implement association getters and setters
 
