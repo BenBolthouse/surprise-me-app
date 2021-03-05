a44981da57a949a5af4cef5a372203bf24d72ba6
benbolthouse <benbolthouse@gmail.com>
2021-03-05 16:26:56 -0700

    Add HOST_NAME as environment variable

--- config/__init__.py
+++ config/__init__.py
@@ -2,6 +2,7 @@ import os
 
 
 class Config:
+    HOST_NAME = os.environ.get('HOST_NAME')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_ECHO = True
