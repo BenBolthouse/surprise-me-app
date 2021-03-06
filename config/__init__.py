0c2b154733451336ba39d89981533ac6bdb54991
benbolthouse <benbolthouse@gmail.com>
2021-03-05 19:55:58 -0700

    Add S3_BUCKET env var to Config

--- config/__init__.py
+++ config/__init__.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     HOST_NAME = os.environ.get('HOST_NAME')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_ECHO = True
