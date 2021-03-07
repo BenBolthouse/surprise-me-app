e886db53ac8428d3f508ecf0507fd29d5e4a8dd6
benbolthouse <benbolthouse@gmail.com>
2021-03-06 19:08:08 -0700

    Import connections into routes module

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,4 +1,5 @@
 from .csrf_routes import csrf_routes
 from .s3_routes import s3_routes
 from .session_routes import session_routes
+from .user_connection_routes import user_connection_routes
 from .user_routes import user_routes
