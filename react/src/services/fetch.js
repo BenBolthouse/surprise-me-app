64ad182f9ac39836cf33714b102aa24132c3a2f9
benbolthouse <benbolthouse@gmail.com>
2021-03-09 16:19:46 -0700

    Correct implementation bug with X-CSRFToken

--- react/src/services/fetch.js
+++ react/src/services/fetch.js
@@ -1,13 +1,16 @@
 import { store } from "../../src";
 
 export const fetch = async (url, options = {}) => {
+  // Get the Redux state
+  const state = store.getState()
+
   options.method = options.method || "GET";
   options.headers = options.headers || {};
 
   if (options.method.toUpperCase() !== "GET") {
     options.headers["Content-Type"] =
       options.headers["Content-Type"] || "application/json";
-    options.headers["XCSRF-Token"] = store.security.xCsrfToken;
+    options.headers["X-CSRFToken"] = state.security.xCsrfToken;
   }
 
   const res = await window.fetch(url, options);
