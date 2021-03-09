b2271dfc3267799e99f2d876285a532d915afafc
benbolthouse <benbolthouse@gmail.com>
2021-03-09 15:48:32 -0700

    Add state csrf token to fetch

--- react/src/services/fetch.js
+++ react/src/services/fetch.js
@@ -1,18 +1,24 @@
-const fetch = async (url, options = {}) => {
+import { store } from "../../src";
+
+export const fetch = async (url, options = {}) => {
   options.method = options.method || "GET";
   options.headers = options.headers || {};
+
   if (options.method.toUpperCase() !== "GET") {
     options.headers["Content-Type"] =
       options.headers["Content-Type"] || "application/json";
+    options.headers["XCSRF-Token"] = store.security.xCsrfToken;
   }
+
   const res = await window.fetch(url, options);
   const contentType = res.headers.get("content-type");
+
   if (contentType && contentType.includes("application/json")) {
     const data = await res.json();
     res.data = data;
   }
+
   if (res.status >= 400) throw res;
+
   return res;
 };
-
-export { fetch };
