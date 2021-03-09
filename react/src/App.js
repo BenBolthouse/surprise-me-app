e95a297039d56cfeac7997435ddeb41c39dd7510
benbolthouse <benbolthouse@gmail.com>
2021-03-09 16:19:23 -0700

    Add useEffect to get state on load

--- react/src/App.js
+++ react/src/App.js
@@ -1,18 +1,26 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import * as securityActions from "./store/reducers/security";
+import * as sessionActions from "./store/reducers/session";
 
 export default () => {
   // Hooks
   const xCsrfToken = useSelector((s) => s.security.xCsrfToken);
   const dispatch = useDispatch();
 
-  // On every render check if the
-  // X-CsrfToken exists. If null then
-  // retrieve one.
-  if (!xCsrfToken) {
-    dispatch(securityActions.getXCsrfToken());
-  }
+  useEffect(() => {
+    // On every render check if the
+    // X-CsrfToken exists. If null then
+    // retrieve one.
+    if (!xCsrfToken) {
+      dispatch(securityActions.getXCsrfToken());
+    }
+
+    // On every render attempt to get
+    // the session user.
+    dispatch(sessionActions.getSessionUser())
+  }, []);
 
   return <h1>Hello world</h1>;
 };
