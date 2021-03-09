da8a3ca2fb1062f3ec7c198240e3775a67b54ca3
benbolthouse <benbolthouse@gmail.com>
2021-03-09 15:32:11 -0700

    Add React index and App component

---
+++ react/src/App.js
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import reducer from "./store/reducers/security";
+
+import * as securityActions from "./store/reducers/security";
+
+export default () => {
+  // Hooks
+  const xCsrfToken = useSelector((s) => s.security.xCsrfToken);
+  const dispatch = useDispatch();
+
+  // On every render check if the
+  // X-CsrfToken exists. If null then
+  // retrieve one.
+  if (!xCsrfToken) {
+    dispatch(securityActions.getXCsrfToken());
+  }
+
+  return <h1>Hello world</h1>;
+};
--- react/src/index.js
+++ react/src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import App from "./App";
+import configureStore from "./store";
+
+const store = configureStore();
 
 // React root entry point
-ReactDOM.render(<div>Render worked!</div>, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
