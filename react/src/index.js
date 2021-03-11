a94df61f6a2a5a6f5311f16cc37986a3899373b1
benbolthouse <benbolthouse@gmail.com>
2021-03-11 14:00:24 -0700

    Rename React files to jsx

--- react/src/index.js
+++ react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import App from "./App";
+import App from "./App.jsx";
 import configureStore from "./store";
 
 import * as sessionActions from "./store/reducers/session";
