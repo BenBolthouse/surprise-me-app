f3b17e0c4df0c891e6ac8f66dc56c1c4de181a97
benbolthouse <benbolthouse@gmail.com>
2021-03-14 13:44:02 -0700

    Correct an error in message seed data

--- seed/chat_messages_seed.py
+++ seed/chat_messages_seed.py
@@ -4,11 +4,11 @@ from models import db, ChatMessage
 
 
 seed = [
-    (6, 1, 4, "Hey, welcome!", "2021-02-05T12:12:00:000Z"),
-    (6, 4, 1, "Thanks! How's it going?", "2021-02-05T12:13:01:000Z"),
-    (6, 1, 4, "Another day another dollar", "2021-02-05T12:13:11:000Z"),
-    (6, 1, 4, "Are you gonna send me a gift??", "2021-02-05T12:13:26:000Z"),
-    (6, 4, 1, "Well I just got here! Patience...", "2021-02-05T12:13:55:000Z"),
+    (6, 1, 35, "Hey, welcome!", "2021-02-05T12:12:00:000Z"),
+    (6, 35, 1, "Thanks! How's it going?", "2021-02-05T12:13:01:000Z"),
+    (6, 1, 35, "Another day another dollar", "2021-02-05T12:13:11:000Z"),
+    (6, 1, 35, "Are you gonna send me a gift??", "2021-02-05T12:13:26:000Z"),
+    (6, 35, 1, "Well I just got here! Patience...", "2021-02-05T12:13:55:000Z"),
 ]
 
 
