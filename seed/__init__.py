385873a800438e395001e5dc1b7d15bffc703b9e
benbolthouse <benbolthouse@gmail.com>
2021-03-14 12:42:27 -0700

    Add messages and connections seeds

--- seed/__init__.py
+++ seed/__init__.py
@@ -1,4 +1,5 @@
 from flask.cli import AppGroup
+from .chat_messages_seed import seed_chat_messages, seed_undo_chat_messages
 from .user_connections_seed import seed_user_connections, seed_undo_user_connections
 from .user_notifications_seed import seed_user_notifications, seed_undo_user_notifications
 from .users_seed import seed_users, seed_undo_users
@@ -12,10 +13,12 @@ def seed_all():
     seed_users()
     seed_user_connections()
     seed_user_notifications()
+    seed_chat_messages()
 
 
 @seed_commands.command("undo")
 def seed_undo_all():
+    seed_undo_chat_messages()
     seed_undo_user_notifications()
     seed_undo_user_connections()
     seed_undo_users()
---
+++ seed/chat_messages_seed.py
@@ -0,0 +1,31 @@
+
+
+from models import db, ChatMessage
+
+
+seed = [
+    (6, 1, 4, "Hey, welcome!", "2021-02-05T12:12:00:000Z"),
+    (6, 4, 1, "Thanks! How's it going?", "2021-02-05T12:13:01:000Z"),
+    (6, 1, 4, "Another day another dollar", "2021-02-05T12:13:11:000Z"),
+    (6, 1, 4, "Are you gonna send me a gift??", "2021-02-05T12:13:26:000Z"),
+    (6, 4, 1, "Well I just got here! Patience...", "2021-02-05T12:13:55:000Z"),
+]
+
+
+def seed_chat_messages():
+    for item in seed:
+        x = ChatMessage({
+            "user_connection_id": item[0],
+            "sender_user_id": item[1],
+            "recipient_user_id": item[2],
+            "body": item[3],
+        })
+        x.established_at = item[4]
+        db.session.add(x)
+        db.session.commit()
+
+
+def seed_undo_chat_messages():
+    db.session.execute('DELETE FROM chat_messages;')
+    db.session.execute('ALTER SEQUENCE chat_messages_id_seq RESTART WITH 1;')
+    db.session.commit()
