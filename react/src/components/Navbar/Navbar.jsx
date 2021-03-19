148f94a7d81112fe283a82827eefab9bfcd53138
benbolthouse <benbolthouse@gmail.com>
2021-03-18 19:13:08 -0700

    Add user map to user modal and style

--- react/src/components/Navbar/Navbar.jsx
+++ react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { Loader } from "react-loaders"
 import {
   BsSearch,
   BsFillChatDotsFill,
   BsGift,
-  BsFillCaretLeftFill
+  BsFillCaretLeftFill,
+  BsPersonFill,
 } from "react-icons/bs";
 import { MdClose } from "react-icons/md";
 import { NavLink } from "react-router-dom";
@@ -13,6 +16,7 @@ import Modal from "../Modal/Modal";
 
 import * as sessionActions from "../../store/reducers/session";
 
+import "loaders.css/src/animations/ball-scale.scss";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -22,10 +26,25 @@ const Navbar = () => {
 
   // Component state
   const [userMenuIsOpen, setUserMenuIsOpen] = useState(false);
+  const [mapLoaded, setMapLoaded] = useState(null);
 
+  const googleMapsAPIKey = process.env.REACT_APP_GOOGLE_API_KEY;
+  const googleMapsDefaultCenter = {
+    lat: parseFloat(sessionUser.coordLat),
+    lng: parseFloat(sessionUser.coordLong),
+  };
+  const googleMapsUserMenuStyles = {
+    height: "12rem",
+    width: "100%",
+  }
+  const googleMapsOptions = {
+    draggable: false,
+    disableDefaultUI: true,
+  }
   const profilePictureSrc64 = `/f/profile_${sessionUser.id}_64p.jpg`;
   const profilePictureSrc128 = `/f/profile_${sessionUser.id}_128p.jpg`;
 
+  // Event handlers
   const logout = (evt) => {
     evt.preventDefault()
     dispatch(sessionActions.logoutSessionUser());
@@ -33,6 +52,13 @@ const Navbar = () => {
   const toggleUserMenu = (evt) => {
     evt.preventDefault();
     setUserMenuIsOpen(!userMenuIsOpen);
+  }
+  const onGoogleMapsMount = () => {
+    dispatch(sessionActions.postSessionGeolocation());
+    setMapLoaded(true);
+  }
+  const onGoogleMapsUnmount = () => {
+    setMapLoaded(false);
   }
 
   return (
@@ -66,7 +92,33 @@ const Navbar = () => {
                   <p className="name">{sessionUser.firstName} {sessionUser.lastName}</p>
                   <p className="email">{sessionUser.email}</p>
                 </div>
+                <div className="user-menu__location">
+                  {!mapLoaded ? 
+                    <div className="map-preloader">
+                      <Loader
+                        type="ball-scale"
+                        color="#555555"/>
+                    </div> : ""  
+                }
+                  <LoadScript googleMapsApiKey={googleMapsAPIKey}>
+                    <GoogleMap
+                      onLoad={onGoogleMapsMount}
+                      onUnmount={onGoogleMapsUnmount}
+                      zoom={13}
+                      mapContainerStyle={googleMapsUserMenuStyles}
+                      center={googleMapsDefaultCenter}
+                      options={googleMapsOptions}>
+                      <Marker position={googleMapsDefaultCenter} />
+                    </GoogleMap>
+                  </LoadScript>
+                </div>
                 <ul className="user-menu__menu">
+                  <li className="edit-profile">
+                    <a href="#" onClick={null} className="inactive">
+                      <BsPersonFill />
+                      <p>Edit Profile</p>
+                    </a>
+                  </li>
                   <li className="logout">
                     <a href="#" onClick={logout}>
                       <BsFillCaretLeftFill />
