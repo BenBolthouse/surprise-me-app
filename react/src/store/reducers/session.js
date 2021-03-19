9365c602f96647bc79cd38a5a8b9c838718d7c4e
benbolthouse <benbolthouse@gmail.com>
2021-03-18 18:32:53 -0700

    Add update geolocation thunk

--- react/src/store/reducers/session.js
+++ react/src/store/reducers/session.js
@@ -1,6 +1,6 @@
 import { fetch } from "../../services/fetch";
 
-import * as securityActions from './security'
+import * as securityActions from "./security";
 
 // State template
 const userTemplate = {
@@ -15,7 +15,7 @@ const userTemplate = {
 
 // ** «««««««««««««««««««««««« Actions »»»»»»»»»»»»»»»»»»»»»»»» **
 
-const LOGIN_SESSION_USER = "session/loginSessionUser";
+const POST_SESSION = "session/postSession";
 /**
  * Establish a session for a user and get the user's data to
  * the Redux store.
@@ -31,55 +31,14 @@ export const loginSessionUser = ({ email, password }) => async (dispatch) => {
   const { data } = res.data;
   dispatch(
     ((payload) => ({
-      type: LOGIN_SESSION_USER,
+      type: POST_SESSION,
       payload,
     }))(data)
   );
   return data;
 };
 
-const LOGOUT_SESSION_USER = "session/logoutSessionUser";
-/**
- * Remove the session for the user and clear Redux state.
- */
-export const logoutSessionUser = () => async (dispatch) => {
-  const res = await fetch("/api/sessions", {
-    method: "DELETE",
-  });
-  const { data } = res.data;
-  dispatch(
-    ((payload) => ({
-      type: LOGOUT_SESSION_USER,
-      payload,
-    }))(data)
-  );
-  dispatch(securityActions.getXCsrfToken())
-  return res;
-};
-
-const POST_SESSION_USER = "session/postSessionUser";
-/**
- * Create a new user and establish a session.
- *
- * @param {*} userObject Contains all of the parameters for
- * a new user.
- */
-export const postSessionUser = (userObject) => async (dispatch) => {
-  const res = await fetch("/api/users", {
-    method: "POST",
-    body: JSON.stringify(userObject),
-  });
-  const { data } = res.data;
-  dispatch(
-    ((payload) => ({
-      type: POST_SESSION_USER,
-      payload,
-    }))(data)
-  );
-  return res;
-};
-
-const GET_SESSION_USER = "session/getSessionUser";
+const GET_SESSION = "session/getSession";
 /**
  * Assuming that the client holds a session token, retrieve
  * the session data and add to Redux state.
@@ -91,26 +50,97 @@ export const getSessionUser = () => async (dispatch) => {
   const { data } = res.data;
   dispatch(
     ((payload) => ({
-      type: GET_SESSION_USER,
+      type: GET_SESSION,
       payload,
     }))(data)
   );
   return res;
 };
 
+const DELETE_SESSION = "session/deleteSession";
+/**
+ * Remove the session for the user and clear Redux state.
+ */
+export const logoutSessionUser = () => async (dispatch) => {
+  const res = await fetch("/api/sessions", {
+    method: "DELETE",
+  });
+  const { data } = res.data;
+  dispatch(
+    ((payload) => ({
+      type: DELETE_SESSION,
+      payload,
+    }))(data)
+  );
+  dispatch(securityActions.getXCsrfToken());
+  return res;
+};
+
+const POST_USER = "session/postSessionUser";
+/**
+ * Create a new user and establish a session.
+ *
+ * @param {*} userObject Contains all of the parameters for
+ * a new user.
+ */
+export const createNewUser = (userObject) => async (dispatch) => {
+  const res = await fetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify(userObject),
+  });
+  const { data } = res.data;
+  dispatch(
+    ((payload) => ({
+      type: POST_USER,
+      payload,
+    }))(data)
+  );
+  return res;
+};
+
+const POST_SESSION_GEOLOCATION = "session/postSessionGeolocation";
+
+export const postSessionGeolocation = () => async (dispatch) => {
+  let geolocation;
+  navigator.geolocation.getCurrentPosition(
+    async ({ coords }) => {
+      await fetch("/api/users", {
+        method: "PATCH",
+        body: JSON.stringify({
+          coordLat: coords.latitude,
+          coordLong: coords.longitude,
+        }),
+      });
+      dispatch(
+        ((payload) => ({
+          type: POST_USER,
+          payload,
+        }))({
+          coordLat: coords.latitude,
+          coordLong: coords.longitude,
+        })
+      );
+      return true;
+    }
+  );
+};
+
 // Reducer
 const reducer = (state = { user: userTemplate }, { type, payload }) => {
   switch (type) {
-    case LOGIN_SESSION_USER:
+    case POST_SESSION:
       return { user: { ...state.user, ...payload } };
 
-    case LOGOUT_SESSION_USER:
+    case DELETE_SESSION:
       return { user: {} };
 
-    case POST_SESSION_USER:
+    case POST_USER:
       return { user: { ...state.user, ...payload } };
 
-    case GET_SESSION_USER:
+    case GET_SESSION:
+      return { user: { ...state.user, ...payload } };
+
+    case POST_SESSION_GEOLOCATION:
       return { user: { ...state.user, ...payload } };
 
     default:
