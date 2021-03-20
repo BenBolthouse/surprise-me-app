41af2490c54623262166f25d32a024b92e4dfa2c
benbolthouse <benbolthouse@gmail.com>
2021-03-20 12:22:01 -0700

    Move user menu out of Navbar component

--- react/src/components/Navbar/Navbar.jsx
+++ react/src/components/Navbar/Navbar.jsx
@@ -10,10 +10,11 @@ import {
 import { MdClose } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Modal from "../Modal/Modal";
 
+import * as modalActions from "../../store/reducers/modal";
 import * as sessionActions from "../../store/reducers/session";
 
 import "loaders.css/src/animations/ball-scale.scss";
@@ -25,40 +26,16 @@ const Navbar = () => {
   const dispatch = useDispatch();
 
   // Component state
-  const [userMenuIsOpen, setUserMenuIsOpen] = useState(false);
-  const [mapLoaded, setMapLoaded] = useState(null);
-
-  const googleMapsAPIKey = process.env.REACT_APP_GOOGLE_API_KEY;
-  const googleMapsDefaultCenter = {
-    lat: parseFloat(sessionUser.coordLat),
-    lng: parseFloat(sessionUser.coordLong),
-  };
-  const googleMapsUserMenuStyles = {
-    height: "12rem",
-    width: "100%",
-  }
-  const googleMapsOptions = {
-    draggable: false,
-    disableDefaultUI: true,
-  }
   const profilePictureSrc64 = `/f/profile_${sessionUser.id}_64p.jpg`;
-  const profilePictureSrc128 = `/f/profile_${sessionUser.id}_128p.jpg`;
 
   // Event handlers
-  const logout = (evt) => {
-    evt.preventDefault()
-    dispatch(sessionActions.logoutSessionUser());
-  }
   const toggleUserMenu = (evt) => {
     evt.preventDefault();
-    setUserMenuIsOpen(!userMenuIsOpen);
-  }
-  const onGoogleMapsMount = () => {
-    dispatch(sessionActions.postSessionGeolocation());
-    setMapLoaded(true);
-  }
-  const onGoogleMapsUnmount = () => {
-    setMapLoaded(false);
+    dispatch(modalActions.setModal({
+      component: <UserMenu
+        onClearCallback={toggleUserMenu} />,
+      clearOnClick: true,
+    }))
   }
 
   return (
@@ -80,58 +57,95 @@ const Navbar = () => {
               <img src={profilePictureSrc64} />
             </a>
           </div>
-          {userMenuIsOpen ?
-            <Modal
-              zIndex="901"
-              clearFunction={toggleUserMenu}
-              clearOnClick={true}>
-              <div className="user-menu">
-                <MdClose className="close" onClick={toggleUserMenu} />
-                <div className="user-menu__header">
-                  <img src={profilePictureSrc128} />
-                  <p className="name">{sessionUser.firstName} {sessionUser.lastName}</p>
-                  <p className="email">{sessionUser.email}</p>
-                </div>
-                <div className="user-menu__location">
-                  {!mapLoaded ? 
-                    <div className="map-preloader">
-                      <Loader
-                        type="ball-scale"
-                        color="#555555"/>
-                    </div> : ""  
-                }
-                  <LoadScript googleMapsApiKey={googleMapsAPIKey}>
-                    <GoogleMap
-                      onLoad={onGoogleMapsMount}
-                      onUnmount={onGoogleMapsUnmount}
-                      zoom={13}
-                      mapContainerStyle={googleMapsUserMenuStyles}
-                      center={googleMapsDefaultCenter}
-                      options={googleMapsOptions}>
-                      <Marker position={googleMapsDefaultCenter} />
-                    </GoogleMap>
-                  </LoadScript>
-                </div>
-                <ul className="user-menu__menu">
-                  <li className="edit-profile">
-                    <a href="#" onClick={null} className="inactive">
-                      <BsPersonFill />
-                      <p>Edit Profile</p>
-                    </a>
-                  </li>
-                  <li className="logout">
-                    <a href="#" onClick={logout}>
-                      <BsFillCaretLeftFill />
-                      <p>Logout</p>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </Modal> : ""
-          }
         </div>
       </div>
     </div>
+  )
+}
+
+const UserMenu = ({ onClearCallback }) => {
+  // Hooks
+  const sessionUser = useSelector(s => s.session.user);
+  const dispatch = useDispatch();
+
+  // Component state
+  const [mapLoaded, setMapLoaded] = useState(null);
+  const googleMapsAPIKey = process.env.REACT_APP_GOOGLE_API_KEY;
+  const googleMapsDefaultCenter = {
+    lat: parseFloat(sessionUser.coordLat),
+    lng: parseFloat(sessionUser.coordLong),
+  };
+  const googleMapsUserMenuStyles = {
+    height: "12rem",
+    width: "100%",
+  }
+  const googleMapsOptions = {
+    draggable: false,
+    disableDefaultUI: true,
+  }
+  const profilePictureSrc128 = `/f/profile_${sessionUser.id}_128p.jpg`;
+
+  // Event handlers
+  const onGoogleMapsMount = (evt) => {
+    dispatch(sessionActions.postSessionGeolocation());
+    setMapLoaded(true);
+  }
+  const onGoogleMapsUnmount = () => {
+    setMapLoaded(false);
+  }
+  const logout = (evt) => {
+    evt.preventDefault()
+    dispatch(sessionActions.logoutSessionUser());
+  }
+  const clearModal = (evt) => {
+    dispatch(modalActions.clearModal());
+  }
+
+  return (
+    <Modal>
+      <div className="user-menu">
+        <MdClose className="close" onClick={clearModal} />
+        <div className="user-menu__header">
+          <img src={profilePictureSrc128} />
+          <p className="name">{sessionUser.firstName} {sessionUser.lastName}</p>
+          <p className="email">{sessionUser.email}</p>
+        </div>
+        <div className="user-menu__location">
+          {!mapLoaded ?
+            <div className="map-preloader">
+              <Loader
+                type="ball-scale"
+                color="#555555" />
+            </div> : ""
+          }
+          <LoadScript googleMapsApiKey={googleMapsAPIKey}>
+            <GoogleMap
+              onLoad={onGoogleMapsMount}
+              onUnmount={onGoogleMapsUnmount}
+              zoom={13}
+              mapContainerStyle={googleMapsUserMenuStyles}
+              center={googleMapsDefaultCenter}
+              options={googleMapsOptions}>
+              <Marker position={googleMapsDefaultCenter} />
+            </GoogleMap>
+          </LoadScript>
+        </div>
+        <ul className="user-menu__menu">
+          <li className="edit-profile">
+            <a href="#" onClick={null} className="inactive">
+              <BsPersonFill />
+              <p>Edit Profile</p>
+            </a>
+          </li>
+          <li className="logout">
+            <a href="#" onClick={logout}>
+              <BsFillCaretLeftFill />
+              <p>Logout</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Modal>
   )
 }
 
