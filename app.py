035bd9b91b50531edabacd40193b608f76f9a665
benbolthouse <benbolthouse@gmail.com>
2021-03-19 17:38:10 -0700

    Create app module for production run

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-from app import app, is_production, socketio
-
-
-import os
-
-
-# Run app with SocketIO
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=os.environ.get('PORT'), debug=False)
+from app import app
