fd35ea38af093d152e46d0cb55ff5ec05242e17f
benbolthouse <benbolthouse@gmail.com>
2021-03-20 12:19:53 -0700

    Add modal reducer to state

--- react/src/store/index.js
+++ react/src/store/index.js
@@ -3,12 +3,14 @@ import { createStore, combineReducers } from "redux";
 import enhancer from "./enhancer";
 
 import connections from './reducers/connections';
+import modal from './reducers/modal';
 import notifications from './reducers/notifications';
 import security from './reducers/security';
 import session from './reducers/session';
 
 const rootReducer = combineReducers({
   connections,
+  modal,
   notifications,
   security,
   session,
