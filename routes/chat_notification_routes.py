e8f79244e02ea5799617a68b3be38c931d5c485c
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-24 11:32:56 -0700

    Correct chat routes and model

--- routes/chat_notification_routes.py
+++ routes/chat_notification_routes.py
@@ -11,21 +11,21 @@ from models import ChatMessage
 chat_notification_routes = Blueprint(
     "chat_notifications",
     __name__,
-    url_prefix="/api/connections")
+    url_prefix="/api/chat_notifications")
 
 
 # ** «««««««««««««««« GET Routes »»»»»»»»»»»»»»»» **
 
 
-@chat_notification_routes.route("/<id>/chat_notifications", methods=["GET"])
+@chat_notification_routes.route("", methods=["GET"])
 @login_required
-def get_chat_notifications(id):
+def get_chat_notifications():
 
     # Get session user
     session_user = current_user
 
     # Jsonify messages
-    notifications = [m.to_json() for m in session_user.notifications]
+    notifications = [m.to_json() for m in session_user.chat_notifications]
 
     # Respond 200 if successful
     return jsonify({
@@ -36,17 +36,39 @@ def get_chat_notifications(id):
 
 # ** «««««««««««««««« DELETE Routes »»»»»»»»»»»»»»»» **
 
-
-@chat_notification_routes.route("/<id>/chat_notifications/<notification_id>", methods=["DELETE"])
+@chat_notification_routes.route("", methods=["DELETE"])
 @login_required
-def delete_chat_notification(id, message_id):
+def delete_chat_notifications(id):
+
+    # Get session user
+    session_user = current_user
 
     # Find the chat notification
-    notification = ChatNotification.query.get(notification_id)
+    notification = ChatNotification.query.filter(
+        ChatNotification.recipient_user_id == session_user.id)
 
     # Then delete it
     db.session.delete(notification)
     db.session.commit()
+    
+    # Respond 200 if successful
+    return jsonify({
+        "message": "Success",
+    }), 200
+
+
+@chat_notification_routes.route("/<id>", methods=["DELETE"])
+@login_required
+def delete_chat_notification(id):
+
+    # Find the chat notification
+    notification = ChatNotification.query.filter(
+        ChatNotification.user_connection_id == int(id)).first()
+
+    # Then delete it
+    if notification:
+        db.session.delete(notification)
+        db.session.commit()
     
     # Respond 200 if successful
     return jsonify({
