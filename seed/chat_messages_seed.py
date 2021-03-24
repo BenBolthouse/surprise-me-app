de8dd923be4bc77a3fb5b540065e466ed5796a95
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-24 00:54:59 -0700

    Add more depth to seed data

--- seed/chat_messages_seed.py
+++ seed/chat_messages_seed.py
@@ -5,17 +5,70 @@ from models import db, ChatMessage
 
 
 seed = [
-    (6, 1, 35, "Hey, welcome!", "2021-02-05T12:12:00.000Z"),
-    (6, 35, 1, "Thanks! How's it going?", "2021-02-05T12:13:01.000Z"),
-    (6, 1, 35, "Another day another dollar", "2021-02-05T12:13:11.000Z"),
-    (6, 1, 35, "Are you gonna send me a gift??", "2021-02-05T12:13:26.000Z"),
-    (6, 35, 1, "Well I just got here! Patience...", "2021-02-05T12:13:55.000Z"),
 
-    (9, 1, 4, "Galvin! My man", "2020-03-14T12:12:00.000Z"),
-    (9, 4, 1, "Sup?", "2021-02-14T12:12:00.000Z"),
-    (9, 1, 4, "Wow really? One year later?", "2021-03-6T12:12:00.000Z"),
-    (9, 4, 1, "Wow dude really? One month later?", "2021-03-13T12:12:00.000Z"),
-    (9, 4, 1, "Alright whateva", "2021-03-14T12:12:00.000Z"),
+    # Charlena Meanwell (1) && Inigo Dur (35)
+    (6, 35, 1, "Hey what's up", "2021-01-05", "13", "12", "05"),
+    (6, 1, 35, "Not much! How's life?", "2021-01-05", "13", "12", "24"),
+    (6, 1, 35, "How's the new place?", "2021-01-05", "13", "12", "27"),
+    (6, 35, 1, "Pretty cool", "2021-01-05", "13", "14", "01"),
+    (6, 35, 1, "Getting accustomed to the new city", "2021-01-05", "13", "14", "12"),
+    (6, 1, 35, "Oh yeah I heard you moved hows SF", "2021-01-05", "13", "15", "22"),
+    (6, 1, 35, "Not too bad lots to see", "2021-01-05", "13", "15", "22"),
+
+    # Charlena Meanwell (1) && Galvin Bogace (4)
+    (9, 4, 1, "Hey what's up", "2021-01-05", "13", "12", "05"),
+    (9, 1, 4, "Not much! How's life?", "2021-01-05", "13", "12", "24"),
+    (9, 1, 4, "How's the new place?", "2021-01-05", "13", "12", "27"),
+    (9, 4, 1, "Pretty cool", "2021-01-05", "13", "14", "01"),
+    (9, 4, 1, "Getting accustomed to the new city", "2021-01-05", "13", "14", "12"),
+    (9, 1, 4, "Oh yeah I heard you moved hows SF", "2021-01-05", "13", "15", "22"),
+    (9, 1, 4, "Not too bad lots to see", "2021-01-05", "13", "15", "22"),
+
+    # Charlena Meanwell (1) && Bryna Sainte Paul (5)
+    (10, 5, 1, "Hey what's up", "2021-01-05", "13", "12", "05"),
+    (10, 1, 5, "Not much! How's life?", "2021-01-05", "13", "12", "24"),
+    (10, 1, 5, "How's the new place?", "2021-01-05", "13", "12", "27"),
+    (10, 5, 1, "Pretty cool", "2021-01-05", "13", "14", "01"),
+    (10, 5, 1, "Getting accustomed to the new city", "2021-01-05", "13", "14", "12"),
+    (10, 1, 5, "Oh yeah I heard you moved hows SF", "2021-01-05", "13", "15", "22"),
+    (10, 1, 5, "Not too bad lots to see", "2021-01-05", "13", "15", "22"),
+
+    # Charlena Meanwell (1) && Alanson Monnoyer (6)
+    (11, 6, 1, "Hey what's up", "2021-01-05", "13", "12", "05"),
+    (11, 1, 6, "Not much! How's life?", "2021-01-05", "13", "12", "24"),
+    (11, 1, 6, "How's the new place?", "2021-01-05", "13", "12", "27"),
+    (11, 6, 1, "Pretty cool", "2021-01-05", "13", "14", "01"),
+    (11, 6, 1, "Getting accustomed to the new city", "2021-01-05", "13", "14", "12"),
+    (11, 1, 6, "Oh yeah I heard you moved hows SF", "2021-01-05", "13", "15", "22"),
+    (11, 1, 6, "Not too bad lots to see", "2021-01-05", "13", "15", "22"),
+
+    # Charlena Meanwell (1) && Etti Restill (7)
+    (12, 7, 1, "Hey what's up", "2021-01-05", "13", "12", "05"),
+    (12, 1, 7, "Not much! How's life?", "2021-01-05", "13", "12", "24"),
+    (12, 1, 7, "How's the new place?", "2021-01-05", "13", "12", "27"),
+    (12, 7, 1, "Pretty cool", "2021-01-05", "13", "14", "01"),
+    (12, 7, 1, "Getting accustomed to the new city", "2021-01-05", "13", "14", "12"),
+    (12, 1, 7, "Oh yeah I heard you moved hows SF", "2021-01-05", "13", "15", "22"),
+    (12, 1, 7, "Not too bad lots to see", "2021-01-05", "13", "15", "22"),
+
+    # Charlena Meanwell (1) && Xena Greguoli (8)
+    (13, 8, 1, "Hey what's up", "2021-01-05", "13", "12", "05"),
+    (13, 1, 8, "Not much! How's life?", "2021-01-05", "13", "12", "24"),
+    (13, 1, 8, "How's the new place?", "2021-01-05", "13", "12", "28"),
+    (13, 8, 1, "Pretty cool", "2021-01-05", "13", "14", "01"),
+    (13, 8, 1, "Getting accustomed to the new city", "2021-01-05", "13", "14", "12"),
+    (13, 1, 8, "Oh yeah I heard you moved hows SF", "2021-01-05", "13", "15", "22"),
+    (13, 1, 8, "Not too bad lots to see", "2021-01-05", "13", "15", "22"),
+
+    # Charlena Meanwell (1) && Rebecca Romain (8)
+    (14, 9, 1, "Hey what's up", "2021-01-05", "13", "12", "05"),
+    (14, 1, 9, "Not much! How's life?", "2021-01-05", "13", "12", "24"),
+    (14, 1, 9, "How's the new place?", "2021-01-05", "13", "12", "29"),
+    (14, 9, 1, "Pretty cool", "2021-01-05", "13", "14", "01"),
+    (14, 9, 1, "Getting accustomed to the new city", "2021-01-05", "13", "14", "12"),
+    (14, 1, 9, "Oh yeah I heard you moved hows SF", "2021-01-05", "13", "15", "22"),
+    (14, 1, 9, "Not too bad lots to see", "2021-01-05", "13", "15", "22"),
+
 ]
 
 
@@ -27,7 +80,8 @@ def seed_chat_messages():
             "recipient_user_id": item[2],
             "body": item[3],
         })
-        x.created_at = datetime.strptime(item[4], "%Y-%m-%dT%H:%M:%S.%fZ")
+        time = f"{item[4]}T{item[5]}:{item[6]}:{item[7]}.000Z"
+        x.created_at = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%fZ")
         db.session.add(x)
         db.session.commit()
 
--- seed/user_connections_seed.py
+++ seed/user_connections_seed.py
@@ -7,38 +7,38 @@ from models import db, UserConnection
 
 
 seed = [
-    (30, 1, None),
-    (31, 1, None),
-    (32, 1, None),
-    (33, 1, datetime.now()),
-    (34, 1, datetime.now()),
-    (35, 1, datetime.now()),
-    (1, 2, None),
-    (1, 3, None),
-    (1, 4, datetime.now()),
-    (1, 5, datetime.now()),
-    (1, 6, datetime.now()),
-    (1, 7, datetime.now()),
-    (1, 8, datetime.now()),
-    (1, 9, datetime.now()),
-    (1, 10, datetime.now()),
-    (1, 12, None),
-    (1, 13, None),
-    (1, 14, datetime.now()),
-    (1, 15, datetime.now()),
-    (1, 16, datetime.now()),
-    (1, 17, None),
-    (1, 18, datetime.now()),
-    (1, 19, datetime.now()),
-    (1, 20, datetime.now()),
-    (1, 22, None),
-    (1, 23, None),
-    (1, 24, datetime.now()),
-    (1, 25, None),
-    (1, 26, datetime.now()),
-    (1, 27, datetime.now()),
-    (1, 28, None),
-    (1, 29, None),
+    (30, 1, None),  # ----------- 1
+    (31, 1, None),  # ----------- 2
+    (32, 1, None),  # ----------- 3
+    (33, 1, datetime.now()),  # - 4
+    (34, 1, datetime.now()),  # - 5
+    (35, 1, datetime.now()),  # - 6
+    (1, 2, None),  # ------------ 7
+    (1, 3, None),  # ------------ 8
+    (1, 4, datetime.now()),  # -- 9
+    (1, 5, datetime.now()),  # -- 10
+    (1, 6, datetime.now()),  # -- 11
+    (1, 7, datetime.now()),  # -- 12
+    (1, 8, datetime.now()),  # -- 13
+    (1, 9, datetime.now()),  # -- 14
+    (1, 10, datetime.now()),  # - 15
+    (1, 12, None),  # ----------- 16
+    (1, 13, None),  # ----------- 17
+    (1, 14, datetime.now()),  # - 18
+    (1, 15, datetime.now()),  # - 19
+    (1, 16, datetime.now()),  # - 20
+    (1, 17, None),  # ----------- 21
+    (1, 18, datetime.now()),  # - 22
+    (1, 19, datetime.now()),  # - 23
+    (1, 20, datetime.now()),  # - 24
+    (1, 22, None),  # ----------- 25
+    (1, 23, None),  # ----------- 26
+    (1, 24, datetime.now()),  # - 27
+    (1, 25, None),  # ----------- 28
+    (1, 26, datetime.now()),  # - 29
+    (1, 27, datetime.now()),  # - 30
+    (1, 28, None),  # ----------- 31
+    (1, 29, None),  # ----------- 32
 ]
 
 
