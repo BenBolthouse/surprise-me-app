1c9fd92283a0763e973a5df66c1c1e329ca5a1d0
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-24 00:52:42 -0700

    Fix side effect bug

--- react/src/components/Chat/Chat.jsx
+++ react/src/components/Chat/Chat.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useParams } from "react-router-dom";
+import { BsArrowLeftShort } from "react-icons/bs";
 
 import ChatThread from "../ChatThread/ChatThread";
 
+import * as sessionActions from "../../store/reducers/session";
+
 import "./Chat.css";
+import ImagePreload from "../ImagePreload/ImagePreload";
 
 const Chat = () => {
   // URL slug
@@ -12,68 +16,131 @@ const Chat = () => {
 
   // Hooks
   const connections = useSelector(s => s.connections);
+  const dispatch = useDispatch();
 
   // Component state
   const [activeMessageThreads, setActiveMessageThreads] = useState(null);
 
+  // side effect gets the users with chat history to a
+  // collection for recent chats and joins the user chat
+  // rooms
   useEffect(() => {
     const messageThreads = [];
-    // eslint-disable-next-line no-unused-vars
-    for (const [key, value] of Object.entries(connections.established)) {
-      if (value.lastChatMessageDatetime) messageThreads.push(value);
+    for (const value of Object.values(connections.established)) {
+      if (value.lastMessage) {
+        messageThreads.push(value)
+        dispatch(sessionActions.joinSocketClientRoom(value.id));
+      };
     }
     setActiveMessageThreads(messageThreads);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [connections.established]);
+  }, [connections]);
 
   return (
     <div className="chat-viewport-container">
-      <div className="view chat-view">
+      <div className="view chat-view page-grid">
         <div className="chat__mobile-nav">
-
+          <NavLink activeClassName="hide" to="/messages/start">
+            <BsArrowLeftShort />
+          </NavLink>
+          <input
+            type="text"
+            name="search-mobile"
+            id="chatRecentsSearchMobile"
+            placeholder="Search" />
         </div>
-        <div className="chat__sidebar">
-          <ul className="chat__threads-available">
-            {activeMessageThreads ?
-              <>
-                {activeMessageThreads.map(messageThread => (
-                  <ThreadAvailable
-                    key={`active-message-thread-${messageThread.id}`}
-                    thread={messageThread} />
-                ))}
-              </> : ""
-            }
-          </ul>
-        </div>
-        {slug !== "start" ?
-          <div className="chat__thread-container">
-            <ChatThread />
-          </div> :
-          <div className="chat__splash-container">
-            <div className="chat__splash">
-              <h1>Messages</h1>
-              <p>Select or search for a friend to start a conversation.</p>
+        <div className="chat__recent-threads">
+          <div>
+            <div className="search">
+              <input
+                type="text"
+                name="search"
+                id="chatRecentsSearch"
+                placeholder="Search" />
             </div>
+            <h2>Recents</h2>
+            <ul>
+              {activeMessageThreads
+                ? activeMessageThreads.map((messageThread) => (
+                  <ThreadAvailable
+                    imageSize="64"
+                    key={`active-message-thread-${messageThread.id}`}
+                    thread={messageThread}
+                  />
+                ))
+                : ""}
+            </ul>
           </div>
-        }
+        </div>
+        <div className="chat__content">
+          {slug !== "start" ?
+            <div>
+              {slug ? <ChatThread /> : ""}
+            </div> :
+            <>
+              <div className="chat__splash">
+                <div>
+                  <h1>Messages</h1>
+                  <p>Select or search for a friend to start a conversation.</p>
+                </div>
+              </div>
+              <div className="chat__splash-mobile">
+                <h2>Recents</h2>
+                <ul>
+                  {activeMessageThreads
+                    ? activeMessageThreads.map((messageThread) => (
+                      <ThreadAvailable
+                        imageSize="128"
+                        key={`active-message-thread-${messageThread.id}`}
+                        thread={messageThread}
+                      />
+                    ))
+                    : ""}
+                </ul>
+              </div>
+            </>
+          }
+        </div>
       </div>
     </div>
   );
 }
 
-const ThreadAvailable = ({ thread }) => {
+const ThreadAvailable = ({ thread, imageSize }) => {
+  // Hooks
+  const sessionUser = useSelector(s => s.session.user);
+  const chatThread = useSelector(s => s.chat[thread.id]);
+
+  // Component state
+  const [lastMessage, setLastMessage] = useState("")
   const otherUserName = `${thread.otherUser.firstName} ${thread.otherUser.lastName}`;
-  const otherUserThumbnail = `/f/profile_${thread.otherUser.id}_64p.jpg`;
-  const otherUserThumbAlt = `User ${thread.otherUser.id} profile picture`;
+  const otherUserThumbnail = `/f/profile_${thread.otherUser.id}_${imageSize}p.jpg`;
   const threadUrl = `/messages/${thread.id}`;
+
+  useEffect(() => {
+    let message = "";
+    if (chatThread && chatThread.lastMessage) {
+      message = chatThread.lastMessage;
+    }
+    else {
+      message = thread.lastMessage;
+    }
+    if (message.sender.id === sessionUser.id) {
+      setLastMessage("Me: " + message.body);
+    }
+    else {
+      setLastMessage(message.body);
+    }
+  }, [chatThread]);
+
   return (
     <NavLink to={threadUrl}>
       <li>
-        <img src={otherUserThumbnail} alt={otherUserThumbAlt} />
-        <p>{otherUserName}</p>
+        <ImagePreload src={otherUserThumbnail} />
+        <p className="name">{otherUserName}</p>
+        <p className="last-message">{lastMessage}</p>
       </li>
     </NavLink>
-  )
+  );
 }
 
 export default Chat;
