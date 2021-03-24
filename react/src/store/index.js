632cc252b784872d6a3ce4094b5cc5410e0a4743
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-24 00:53:26 -0700

    Change notifications reducer to localstorage

--- react/src/store/index.js
+++ react/src/store/index.js
@@ -4,16 +4,16 @@ import enhancer from "./enhancer";
 
 import chat from './reducers/chat';
 import connections from './reducers/connections';
+import localStorage from './reducers/localStorage';
 import modal from './reducers/modal';
-import notifications from './reducers/notifications';
 import security from './reducers/security';
 import session from './reducers/session';
 
 const rootReducer = combineReducers({
   chat,
   connections,
+  localStorage,
   modal,
-  notifications,
   security,
   session,
 });
