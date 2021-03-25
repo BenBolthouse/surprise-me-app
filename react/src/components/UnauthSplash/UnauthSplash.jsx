b419510f08d6c15a2524c0febac4e2e2b52202f8
Ben Bolthouse <benbolthouse@gmail.com>
2021-03-25 13:24:09 -0700

    Change layout and cosmetics

--- react/src/components/UnauthSplash/UnauthSplash.jsx
+++ react/src/components/UnauthSplash/UnauthSplash.jsx
@@ -26,21 +26,21 @@ const UnauthSplash = () => {
         </div>
       </div>
       <div className="unauth-splash__details">
-        <div className="unauth-splash__details-bucket-a">
-          <img src="/f/unauth_bucket_a_badge_320.svg" alt="" />
+        <div className="bucket">
+          <object data="/images/unauth_bucket_a_badge_320p.svg" type="image/svg+xml" />
           <p>Find a perfect gift for a friend from local businesses</p>
         </div>
-        <div className="unauth-splash__details-bucket-b">
-          <img src="/f/unauth_bucket_b_badge_320.svg" alt="" />
+        <div className="bucket">
+          <object data="/images/unauth_bucket_b_badge_320p.svg" type="image/svg+xml" />
           <p>Send a friend on the hunt to find the gift</p>
         </div>
-        <div className="unauth-splash__details-bucket-c">
-          <img src="/f/unauth_bucket_c_badge_320.svg" alt="" />
+        <div className="bucket">
+          <object data="/images/unauth_bucket_c_badge_320p.svg" type="image/svg+xml" />
           <p>They won't discover what the surprise is until they find it!</p>
         </div>
       </div>
       <div className="unauth-splash__cta">
-        <h2>Are you in?</h2>
+        <h2>Pretty simple, huh?</h2>
         <p>Sign up to send a gift to a friend!</p>
       </div>
     </div>
