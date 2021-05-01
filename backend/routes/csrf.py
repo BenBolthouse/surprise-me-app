46190ca58f28c99c8b241699f3278af2a948da0a
Ben Bolthouse <benbolthouse@gmail.com>
2021-04-30 17:22:12 -0700

    Change http method from POST to GET

--- backend/routes/csrf.py
+++ backend/routes/csrf.py
@@ -7,7 +7,7 @@ csrf_routes = Blueprint("csrf_routes", __name__, url_prefix="/api/v1/csrf_token"
 
 # GET https://surprise-me.benbolt.house/api/v1/csrf_token
 # Issues a new anti CSRF token to the client in a JSON body.
-@csrf_routes.route("", methods=["POST"])
+@csrf_routes.route("", methods=["GET"])
 def post():
     return jsonify({
         "message": "Success",
