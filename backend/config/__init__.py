54e6e034cd56057523ae983e01c5a88fe48655cb
Ben Bolthouse <benbolthouse@gmail.com>
2021-04-30 17:21:18 -0700

    Refactor for test environment changes

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     AWS_S3_BUCKET = os.environ.get('AWS_S3_BUCKET')
-    HOST_NAME = os.environ.get('PUBLIC_URL')
+    PUBLIC_URL = os.environ.get('PUBLIC_URL')
     IS_PRODUCTION_ENV = os.environ.get('ENVIRONMENT') == 'production'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
