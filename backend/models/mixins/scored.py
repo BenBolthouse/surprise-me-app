3c27de76776e9b8c8032e4ef14c17fe2f2fec35a
Ben Bolthouse <benbolthouse@gmail.com>
2021-04-30 21:18:30 -0700

    Fix for divide by zero exception

--- backend/models/mixins/scored.py
+++ backend/models/mixins/scored.py
@@ -38,8 +38,13 @@ class ScoredMixin(object):
         is a percentage grade of engagement weighted against the user's total
         engagement for the specific category.
         '''
-        self._score += points
-        self._grade = user_score / self._score * 100
+        if self._score + points < 1:
+            self._score = 0
+            self._grade = 0
+
+        else:
+            self._score += points
+            self._grade = user_score / self._score * 100
 
     def __init__(self):
         self._score = 0
