89455745d9f7e964614d23227682c8b643cb599c
Ben Bolthouse <benbolthouse@gmail.com>
2021-04-30 21:18:04 -0700

    Add new cluster

--- backend/seed/test/users.py
+++ backend/seed/test/users.py
@@ -1,30 +1,46 @@
 from models import db, Email, Password, User
 
+PASSWORD = "Password1234$"
 
 seed = [
+    # Cluster 1
     {
         "first_name": "UserA",
         "last_name": "UserA",
         "email": "A@email.com",
-        "password": "Password1234$",
+        "password": PASSWORD,
     },
     {
         "first_name": "UserB",
         "last_name": "UserB",
         "email": "B@email.com",
-        "password": "Password1234$",
+        "password": PASSWORD,
     },
     {
         "first_name": "UserC",
         "last_name": "UserC",
         "email": "C@email.com",
-        "password": "Password1234$",
+        "password": PASSWORD,
     },
     {
         "first_name": "UserD",
         "last_name": "UserD",
         "email": "D@email.com",
-        "password": "Password1234$",
+        "password": PASSWORD,
+    },
+
+    # Cluster 2
+    {
+        "first_name": "UserE",
+        "last_name": "UserE",
+        "email": "E@email.com",
+        "password": PASSWORD,
+    },
+    {
+        "first_name": "UserF",
+        "last_name": "UserF",
+        "email": "F@email.com",
+        "password": PASSWORD,
     },
 ]
 
