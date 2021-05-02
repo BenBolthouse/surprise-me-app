f4762b97d5a2b090cdd2859703e28cf2a92c721f
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-02 15:28:25 -0700

    Remove scored entity integrations for now

--- backend/seed/__init__.py
+++ backend/seed/__init__.py
@@ -5,7 +5,6 @@ from .test.connections import up as test_connections_up
 from .undo.connections import down as connections_down
 from .test.users import up as test_users_up
 from .undo.users import down as users_down
-from .undo.scored_users import down as scored_users_down
 
 
 seed_commands = AppGroup("seed")
@@ -18,5 +17,4 @@ def test():
 
 def down():
     connections_down()
-    scored_users_down()
     users_down()
