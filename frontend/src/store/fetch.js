00e07e8b29df02a4571b03f750226641f784fd0a
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-04 19:51:17 -0700

    Refactor fetch to use default options

--- frontend/src/store/fetch.js
+++ frontend/src/store/fetch.js
@@ -1,23 +1,42 @@
+import { store } from "../index";
+
+const defaults = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+};
+
 /**
  * Base fetch API customized for connection to the service API.
- * 
+ *
  * @param {String} url URL for the request
  * @param {object} options Config object for fetch API function
  */
 export const fetch = async (url, options = {}) => {
-  const notGetRequest = options.method.toUpperCase() !== "GET";
-  options.method = options.method || "GET";
-  options.headers = options.headers || {};
+  const request = { ...defaults, ...options };
 
-  if (notGetRequest) {
-    options.headers["Content-Type"] = "application/json";
-    options.headers["X-CSRFToken"] = "";
+  // Following attempts to find the csrf token in the Redux state with
+  // every state-changing API request (POST, PATCH, PUT, DELETE). If the
+  // csrf token returns null then the function throws an error.
+  const csrfToken = store.getState().session._csrfToken;
+
+  if (request.method.toUpperCase() !== "GET") {
+    // Throw an exception if the Redux state does not have a valid csrf
+    // token.
+    if (!csrfToken) {
+      throw Error("Invalid or missing security token.");
+    }
+    request.body = JSON.stringify(options.body);
+    request.headers["X-CSRFToken"] = csrfToken;
   }
 
-  const res = await window.fetch(url, options);
-  const { message, data } = await res.json();
+  const response = await window.fetch(url, request);
+  const body = await response.json();
+  const { message, data } = body;
 
-  if (res.status >= 400) throw res;
+  if (response.status >= 400) throw response;
 
   return { message, data };
 };
