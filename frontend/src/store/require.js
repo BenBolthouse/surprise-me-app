1f18d1ae68b7e37b5ca97aca6256b68d9aa6d89a
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-04 19:52:23 -0700

    Add require decorators

---
+++ frontend/src/store/require.js
@@ -0,0 +1,30 @@
+import { store } from "../index";
+
+/**
+ * Determines if a session has been established by reading the Redux state
+ * for a session id. Returns the session object or throws an error if not
+ * found.
+ */
+export function requireSession() {
+  const session = store.getState().session;
+
+  if (!session._id) {
+    throw new Error("A session is required to utilize this reducer.");
+  }
+  
+  return session;
+}
+
+/**
+ * Determines if a the csrf token is present. Returns true or throws an
+ * error if not found.
+ */
+export function requireCsrf() {
+  const token = store.getState().session._csrfToken;
+
+  if (!token) {
+    throw new Error("A csrf token is required to send this request.");
+  }
+  
+  return true;
+}
