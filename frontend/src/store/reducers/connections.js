a666ad822d200d3b4916830054839f9d70f15c69
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-04 19:54:26 -0700

    Add reducers for new entity models

--- frontend/src/store/reducers/connections.js
+++ frontend/src/store/reducers/connections.js
@@ -1,5 +1,6 @@
-import { fetch } from "../fetch";
 import { Connection, ConnectionsCollection } from "../models/connection";
+import { fetch } from "../fetch";
+import { requireSession, requireCsrf } from "../require";
 
 const connections = new ConnectionsCollection();
 
@@ -7,71 +8,114 @@ const POST_CONNECTION = "connections ———————> POST_CONNECTION";
 const GET_CONNECTIONS = "connections ———————> GET_CONNECTIONS";
 const APPROVE_CONNECTION = "connections ———————> APPROVE_CONNECTION";
 const DENY_CONNECTION = "connections ———————> DENY_CONNECTION";
-const LEAVE_CONNECTION = "connections ———————> DENY_CONNECTION";
+const LEAVE_CONNECTION = "connections ———————> LEAVE_CONNECTION";
 
-export const postConnection = (id) => async (dispatch) => {
-  const body = { "approver_id": id }
+export const postConnection = ({ approverId }) => async (dispatch) => {
+  requireCsrf();
+  requireSession();
+
+  const body = { approver_id: approverId };
   const { data } = await fetch(connections.endpoint, { method: "POST", body });
-  dispatch(() => ({ type: POST_CONNECTION, payload: data }));
-  return true;
+
+  dispatch(postConnectionAction(data));
 };
+
+const postConnectionAction = (payload) => ({
+  type: POST_CONNECTION,
+  payload,
+});
 
 export const getConnections = () => async (dispatch) => {
-  const { data } = await fetch(connections.endpoint, { method: "GET" });
-  dispatch(() => ({ type: GET_CONNECTIONS, payload: data }));
-  return true;
+  connections.userId = requireSession()._id;
+
+  let { data } = await fetch(connections.endpoint, { method: "GET" });
+
+  dispatch(getConnectionsAction(data));
 };
 
-export const approveConnection = (id) => async (dispatch) => {
+const getConnectionsAction = (payload) => ({
+  type: GET_CONNECTIONS,
+  payload,
+});
+
+export const approveConnection = ({ id }) => async (dispatch) => {
+  requireCsrf();
+  requireSession();
+
   const endpoint = `${connections.endpoint}/${id}/approve`;
+
   const { data } = await fetch(endpoint, { method: "PATCH" });
-  dispatch(() => ({ type: APPROVE_CONNECTION, payload: data }));
+  dispatch(approveConnectionAction(data));
+
   return true;
 };
 
-export const denyConnection = (id) => async (dispatch) => {
+const approveConnectionAction = (payload) => ({
+  type: APPROVE_CONNECTION,
+  payload,
+});
+
+export const denyConnection = ({ id }) => async (dispatch) => {
+  requireCsrf();
+  requireSession();
+
   const endpoint = `${connections.endpoint}/${id}/deny`;
+
   const { data } = await fetch(endpoint, { method: "PATCH" });
-  dispatch(() => ({ type: DENY_CONNECTION, payload: data }));
+  dispatch(denyConnectionAction(data));
+
   return true;
 };
 
-export const leaveConnection = (id) => async (dispatch) => {
+const denyConnectionAction = (payload) => ({
+  type: DENY_CONNECTION,
+  payload,
+});
+
+export const leaveConnection = ({ id }) => async (dispatch) => {
+  requireCsrf();
+  requireSession();
+
   const endpoint = `${connections.endpoint}/${id}`;
+
   const { data } = await fetch(endpoint, { method: "DELETE" });
-  dispatch(() => ({ type: LEAVE_CONNECTION, payload: data }));
+  dispatch(leaveConnectionAction(data));
+
   return true;
 };
 
-const reducer = (state = connections, { type, payload }) => {
-  let connectionsCopy = { ...state };
+const leaveConnectionAction = (payload) => ({
+  type: LEAVE_CONNECTION,
+  payload,
+});
+
+const reducer = (state = connections.state(), { type, payload }) => {
   let connection;
 
   switch (type) {
     case POST_CONNECTION:
-      connection = new Connection();
-      connection.populate(payload);
-      connectionsCopy.add(connection);
-      return connectionsCopy;
+      connection = new Connection(connections.endpoint);
+      connection.populateEntity(payload);
+      connections.addRequested(connection);
+      return connections.state();
 
     case GET_CONNECTIONS:
-      connectionsCopy.populate(payload);
-      return connectionsCopy;
+      connections.populateCollection(payload);
+      return connections.state();
 
     case APPROVE_CONNECTION:
-      connection = connectionsCopy.filter(x => x.id === payload.id)
-      connection.populate(payload);
-      return connectionsCopy;
+      connection = connections.filter((x) => x.id === payload.id)[payload.id];
+      connection.populateEntity(payload);
+      connections.movePendingToApproved(payload);
+      return connections.state();
 
     case DENY_CONNECTION:
-      connection = connectionsCopy.filter(x => x.id === payload.id)
-      connection.populate(payload);
-      return connectionsCopy;
+      connections.removeFromAllCollections(payload);
+      return connections.state();
 
     case LEAVE_CONNECTION:
-      connection = connectionsCopy.filter(x => x.id === payload.id)
-      connection.populate(payload);
-      return connectionsCopy;
+      connections.removeFromAllCollections(payload);
+      return connections.state();
 
     default:
       return state;
--- frontend/src/store/reducers/session.js
+++ frontend/src/store/reducers/session.js
@@ -1,37 +1,75 @@
 import { fetch } from "../fetch";
 import { Session } from "../models/session";
+import { requireCsrf } from "../require";
 
-const session = new Session("/api/v1/sessions");
+const session = new Session("/api/v1/sessions", "/api/v1/csrf_token");
 
 const GET_CSRF_TOKEN = "session ———————————> GET_CSRF_TOKEN";
 const POST_SESSION = "session ———————————> POST_SESSION";
-// const GET_SESSION = "session ———————————> GET_SESSION";
-// const DELETE_SESSION = "session ———————————> DELETE_SESSION";
+const GET_SESSION = "session ———————————> GET_SESSION";
+const DELETE_SESSION = "session ———————————> DELETE_SESSION";
 
 export const getCsrfToken = () => async (dispatch) => {
-  const { data } = await fetch(session.endpoint, { method: "GET" });
-  dispatch(() => ({ type: GET_CSRF_TOKEN, payload: data }));
-  return true;
+  const { data } = await fetch(session.csrfEndpoint, { method: "GET" });
+  dispatch(getCsrfTokenAction(data));
 };
 
-export const postSession = (email, password) => async (dispatch) => {
+const getCsrfTokenAction = (payload) => ({
+  type: GET_CSRF_TOKEN,
+  payload,
+});
+
+export const postSession = ({ email, password }) => async (dispatch) => {
+  requireCsrf();
+
   const body = { email, password };
   const { data } = await fetch(session.endpoint, { method: "POST", body });
-  dispatch(() => ({ type: POST_SESSION, payload: data }));
-  return true;
+
+  dispatch(postSessionAction(data));
 };
 
-const reducer = (state = session, { type, payload }) => {
-  let sessionCopy = { ...state };
+const postSessionAction = (payload) => ({
+  type: POST_SESSION,
+  payload,
+});
 
+export const getSession = () => async (dispatch) => {
+  const { data } = await fetch(session.endpoint, { method: "GET" });
+
+  dispatch(getSessionAction(data));
+};
+
+const getSessionAction = (payload) => ({
+  type: GET_SESSION,
+  payload,
+});
+
+export const deleteSession = () => async (dispatch) => {
+  requireCsrf();
+  
+  await fetch(session.endpoint, { method: "DELETE" });
+
+  dispatch(deleteSessionAction());
+};
+
+const deleteSessionAction = (payload) => ({
+  type: DELETE_SESSION,
+  payload,
+});
+
+const reducer = (state = session.state(), { type, payload }) => {
   switch (type) {
     case GET_CSRF_TOKEN:
-      sessionCopy.setCsrfToken(payload);
-      return sessionCopy;
-    
+      session.setCsrfToken(payload);
+      return session.state();
+
     case POST_SESSION:
-      sessionCopy.populate(payload);
-      return sessionCopy;
+      session.populateEntity(payload);
+      return session.state();
+
+    case GET_SESSION:
+      session.populateEntity(payload);
+      return session.state();
 
     default:
       return state;
