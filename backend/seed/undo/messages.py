20f3c8fba0267513800ec0cbc158af540869810d
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-04 19:50:20 -0700

    Add db commit to seed undo

--- backend/seed/undo/messages.py
+++ backend/seed/undo/messages.py
@@ -4,3 +4,4 @@ from models import db
 def down():
     db.session.execute('DELETE FROM messages;')
     db.session.execute('ALTER SEQUENCE messages_id_seq RESTART WITH 1;')
+    db.session.commit()
--- backend/seed/undo/users.py
+++ backend/seed/undo/users.py
@@ -4,3 +4,4 @@ from models import db
 def down():
     db.session.execute('DELETE FROM users;')
     db.session.execute('ALTER SEQUENCE users_id_seq RESTART WITH 1;')
+    db.session.commit()
