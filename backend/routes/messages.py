3584a3270d0034c084624b90269224485d26f558
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-05 23:40:17 -0700

    Implement POST route

--- backend/routes/messages.py
+++ backend/routes/messages.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from flask_login import current_user, login_required
-from sqlalchemy import desc, or_
+from flask_socketio import emit
+from socketio.exceptions import SocketIOError
+from sqlalchemy import and_, desc, or_
 from werkzeug.exceptions import BadRequest, Forbidden
 
 from app import socketio
@@ -10,10 +12,56 @@ from models import db, Connection, Message
 message_routes = Blueprint("message_routes", __name__, url_prefix="/api/v1/connections")
 
 
+# POST https://surprise-me.benbolt.house/api/v1/connections/<connection_id>/messages
+# Creates a new message for the given connection and handles emitting the
+# message to connected parties.
+@message_routes.route("/<connection_id>/messages", methods=["POST"])
+def post(connection_id):
+    get = request.json.get
+
+    body = get("body")
+
+    connection = Connection.query.filter(
+        and_(
+            or_(
+                Connection._requestor_id == current_user.id,
+                Connection._approver_id == current_user.id),
+            Connection._id == int(connection_id))).first()
+
+    # handle bad requests for non-member users
+    if not connection:
+        raise BadRequest(response={
+            "message": "User not connected to recipient user",
+        })
+
+    other_user = connection.other_user(current_user.id)
+    recipient_id = other_user["id"]
+
+    message_room_name = f"message_room_{recipient_id}"
+
+    message = Message(int(connection_id), current_user.id, recipient_id, body)
+
+    db.session.add(message)
+    db.session.commit()
+
+    response = {
+        "message": "Success",
+        "data": {
+            "connection_id": int(connection_id),
+            "messages": [message.to_dict()],
+        }
+    }
+
+    # emit to user's message room
+    socketio.emit("deliver_message", response, to=message_room_name)
+
+    return jsonify(response), 201
+
+
 # GET https://surprise-me.benbolt.house/api/v1/connections/<connection_id>/messages
 # Retrieves a set and offset quantity of messages based on url queries.
-@message_routes.route("/<connection_id>/messages", methods=["GET"])
-@login_required
+@ message_routes.route("/<connection_id>/messages", methods=["GET"])
+@ login_required
 def get(connection_id):
     offset = request.args.get("ofs")
     limit = request.args.get("lim")
@@ -25,9 +73,11 @@ def get(connection_id):
         })
 
     connection = Connection.query.filter(
-        or_(
-            Connection._requestor_id == current_user.id,
-            Connection._approver_id == current_user.id)).first()
+        and_(
+            or_(
+                Connection._requestor_id == current_user.id,
+                Connection._approver_id == current_user.id),
+            Connection._id == int(connection_id))).first()
 
     # handle bad requests for non-member users
     if not connection:
@@ -37,7 +87,7 @@ def get(connection_id):
 
     # get offset limit of messages and format for json response
     messages = Message.query.filter(Message._connection_id == int(connection_id))\
-        .order_by(Message._created_at.desc())\
+        .order_by(Message._id.desc())\
         .offset(offset)\
         .limit(limit)\
         .all()
