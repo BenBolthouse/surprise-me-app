610f17271d4d634529c04ae8923dd9522a76c16f
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-05 23:56:43 -0700

    Implement post message thunk

--- frontend/src/store/reducers/messages.js
+++ frontend/src/store/reducers/messages.js
@@ -1,37 +1,93 @@
-import { MessageManager } from "../models/message";
 import { fetch } from "../utilities/fetch";
+import { MessageManager } from "../models/Message";
 import { requires, socket } from "../index";
 
 const messagesManager = new MessageManager();
 
-const LIMIT = 5;
+const LIMIT = 20;
 
-// const POST_MESSAGE = "messages ——————————> POST_MESSAGE"
+const POST_MESSAGE = "messages ——————————> POST_MESSAGE"
+const RECEIVE_MESSAGE = "messages ——————————> RECEIVE_MESSAGE"
 const GET_MESSAGES = "messages ——————————> GET_MESSAGES";
 // const UPDATE_MESSAGE = "messages ——————————> UPDATE_MESSAGE"
 // const DELETE_MESSAGE = "messages ——————————> DELETE_MESSAGE"
 // const COMPOSE_MESSAGE = "messages ——————————> COMPOSE_MESSAGE"
 // const ABANDON_MESSAGE = "messages ——————————> ABANDON_MESSAGE"
 
-export const getMessages = ({ connectionId }) => async (dispatch) => {
-  requires.session();
+/**
+ * Posts a new message.
+ * 
+ * @param {Object} config
+ */
+export const postMessage = ({ connectionId, type, body: msgBody }) => async (dispatch) => {
+  const sessionId = requires.session().id;
+  requires.csrf();
   socket.require();
 
-  const collection = messagesManager.syncCollectionOffset(connectionId, LIMIT); // prettier-ignore
-  const offset = collection ? collection.offset : 0;
+  const collection = messagesManager.getOrCreateMessageCollection(connectionId);
   
-  const query = `?ofs=${offset}&lim=${LIMIT}`;
+  collection.offset ++;
+  
+  const body = {
+    sender_id: sessionId,
+    connection_id: connectionId,
+    type,
+    body: msgBody,
+  }
 
-  const endpoint = messagesManager.endpoint + `/${connectionId}/messages${query}`;
+  const { data } = await fetch(collection.endpoint, { method: "POST", body });
+
+  dispatch(postMessageAction(data));
+};
+
+const postMessageAction = (payload) => ({ type: POST_MESSAGE, payload });
+
+/**
+ * Socketio event receive new message.
+ * 
+ * @param {Object} payload Message object from event
+ */
+export const receiveMessageAction = ({ data }) => {
+  const collection = messagesManager.getOrCreateMessageCollection(data.connection_id);
+
+  collection.offset ++;
+  
+  return { type: RECEIVE_MESSAGE, payload: data }
+};
+
+/**
+ * Gets new messages by offset limit.
+ * 
+ * @param {Object} config
+ */
+export const getMessages = ({ connectionId }) => async (dispatch) => {
+  requires.session().id;
+
+  const collection = messagesManager.getOrCreateMessageCollection(connectionId);
+
+  const query = `?ofs=${collection.offset}&lim=${LIMIT}`;
+
+  collection.offset += LIMIT;
+  
+  const endpoint = collection.endpoint + query;
+
   const { data } = await fetch(endpoint, { method: "GET" });
 
   dispatch(getMessagesAction(data));
 };
 
-const getMessagesAction = (payload) => ({type: GET_MESSAGES, payload });
+const getMessagesAction = (payload) => ({ type: GET_MESSAGES, payload });
 
 const reducer = (state = messagesManager.state(), { type, payload }) => {
   switch (type) {
+    case POST_MESSAGE:
+      messagesManager.populateCollection(payload);
+      return messagesManager.state();
+
+    case RECEIVE_MESSAGE:
+      messagesManager.populateCollection(payload);
+      return messagesManager.state();
+
     case GET_MESSAGES:
       messagesManager.populateCollection(payload);
       return messagesManager.state();
