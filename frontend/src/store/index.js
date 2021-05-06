2183185a6d2a39da1dcf494146ffa5c58113d8b6
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-05 23:41:56 -0700

    Add service singletons and messages reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,20 @@ import { createStore, combineReducers } from "redux";
 
 import enhancer from "./enhancer";
 
+import Require from "./utilities/require"
+import Socket from "./utilities/socket"
+
+export const requires = new Require();
+export const socket = new Socket();
+
 import connections from "./reducers/connections";
+import messages from "./reducers/messages";
 import session from "./reducers/session";
 
 const rootReducer = combineReducers({
-  connections,
   session,
+  connections,
+  messages,
 });
 
 const configureStore = (preloadedState) =>
