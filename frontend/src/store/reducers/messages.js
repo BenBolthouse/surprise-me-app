02a7d5810d346c9dafc28d0d2dc9296119ac8b50
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-06 04:56:24 -0700

    Add actions for mesage events

--- frontend/src/store/reducers/messages.js
+++ frontend/src/store/reducers/messages.js
@@ -6,15 +6,16 @@ const messagesManager = new MessageManager();
 
 const LIMIT = 20;
 
-const POST_MESSAGE = "messages ——————————> POST_MESSAGE"
+// http actions
+const POST_MESSAGE = "messages ——————————> POST_MESSAGE";
 const GET_MESSAGES = "messages ——————————> GET_MESSAGES";
 const PATCH_MESSAGE = "messages ——————————> UPDATE_MESSAGE";
-const RECEIVE_MESSAGE = "messages ——————————> RECEIVE_MESSAGE"
-const AMEND_MESSAGE = "messages ——————————> RECEIVE_MESSAGE"
-// const UPDATE_MESSAGE = "messages ——————————> UPDATE_MESSAGE"
-// const DELETE_MESSAGE = "messages ——————————> DELETE_MESSAGE"
-// const COMPOSE_MESSAGE = "messages ——————————> COMPOSE_MESSAGE"
-// const ABANDON_MESSAGE = "messages ——————————> ABANDON_MESSAGE"
+const DELETE_MESSAGE = "messages ——————————> DELETE_MESSAGE";
+
+// event actions
+const RECEIVE_MESSAGE = "messages ——————————> RECEIVE_MESSAGE";
+const AMEND_MESSAGE = "messages ——————————> AMEND_MESSAGE";
+const DISCARD_MESSAGE = "messages ——————————> DISCARD_MESSAGE";
 
 /**
  * Posts a new message.
@@ -44,6 +45,29 @@ export const postMessage = ({ connectionId, type, body: msgBody }) => async (dis
 const postMessageAction = (payload) => ({ type: POST_MESSAGE, payload });
 
 /**
+ * Gets new messages by offset limit.
+ * 
+ * @param {Object} config
+ */
+export const getMessages = ({ connectionId }) => async (dispatch) => {
+  requires.session().id;
+
+  const collection = messagesManager.getOrCreateMessageCollection(connectionId);
+
+  const query = `?ofs=${collection.offset}&lim=${LIMIT}`;
+
+  collection.offset += LIMIT;
+  
+  const endpoint = collection.endpoint + query;
+
+  const { data } = await fetch(endpoint, { method: "GET" });
+
+  dispatch(getMessagesAction(data));
+};
+
+const getMessagesAction = (payload) => ({ type: GET_MESSAGES, payload });
+
+/**
  * Updates a message.
  * 
  * @param {Object} config
@@ -71,12 +95,33 @@ export const patchMessage = ({ connectionId, id, type, body: msgBody }) => async
 const patchMessageAction = (payload) => ({ type: PATCH_MESSAGE, payload });
 
 /**
+ * Deletes a message.
+ * 
+ * @param {Object} config
+ */
+export const deleteMessage = ({ connectionId, id }) => async (dispatch) => {
+  requires.session();
+  requires.csrf();
+  socket.require();
+
+  const collection = messagesManager.getOrCreateMessageCollection(connectionId);
+
+  const endpoint = `${collection.endpoint}/${id}`
+  
+  const { data } = await fetch(endpoint, { method: "DELETE" });
+
+  dispatch(deleteMessageAction(data));
+};
+
+const deleteMessageAction = (payload) => ({ type: DELETE_MESSAGE, payload });
+
+/**
  * Socketio event receive new message.
  * 
  * @param {Object} payload Message object from event
  */
 export const receiveMessage = ({ data }) => {
-  const collection = messagesManager.getOrCreateMessageCollection(data.connection_id);
+  const collection = messagesManager.getMessageCollection(data.connection_id);
 
   collection.offset ++;
   
@@ -88,32 +133,37 @@ export const receiveMessage = ({ data }) => {
  * 
  * @param {Object} payload Message object from event
  */
-export const amendMessage = ({ data }) => ({ type: AMEND_MESSAGE, payload: data });
-
-/**
- * Gets new messages by offset limit.
- * 
- * @param {Object} config
- */
-export const getMessages = ({ connectionId }) => async (dispatch) => {
-  requires.session().id;
-
-  const collection = messagesManager.getOrCreateMessageCollection(connectionId);
-
-  const query = `?ofs=${collection.offset}&lim=${LIMIT}`;
-
-  collection.offset += LIMIT;
+export const amendMessage = ({ data }) => {
+  const collection = messagesManager.getMessageCollection(data.connection_id);
   
-  const endpoint = collection.endpoint + query;
+  const message = collection ? collection.getMessage(data.messages[0].id) : null;
 
-  const { data } = await fetch(endpoint, { method: "GET" });
+  if (message) {
+    return { type: AMEND_MESSAGE, payload: data }
+  }
 
-  dispatch(getMessagesAction(data));
+  return { type: "messages ——————————> NO_OP" }
 };
 
-const getMessagesAction = (payload) => ({ type: GET_MESSAGES, payload });
+/**
+ * Socketio event delete and remove a message.
+ * 
+ * @param {Object} payload Message object from event
+ */
+export const discardMessage = ({ data }) => {
+  const collection = messagesManager.getMessageCollection(data.connection_id);
+  
+  const message = collection ? collection.getMessage(data.messages[0].id) : null;
+
+  if (message) {
+    return { type: DISCARD_MESSAGE, payload: data }
+  }
+
+  return { type: "messages ——————————> NO_OP" }
+};
 
 const reducer = (state = messagesManager.state(), { type, payload }) => {
+  
   switch (type) {
     case POST_MESSAGE:
       messagesManager.populateCollection(payload);
@@ -123,11 +173,23 @@ const reducer = (state = messagesManager.state(), { type, payload }) => {
       messagesManager.populateCollection(payload);
       return messagesManager.state();
 
+    case GET_MESSAGES:
+      messagesManager.populateCollection(payload);
+      return messagesManager.state();
+
+    case DELETE_MESSAGE:
+      messagesManager.populateCollection(payload);
+      return messagesManager.state();
+
     case RECEIVE_MESSAGE:
       messagesManager.populateCollection(payload);
       return messagesManager.state();
 
-    case GET_MESSAGES:
+    case AMEND_MESSAGE:
+      messagesManager.populateCollection(payload);
+      return messagesManager.state();
+
+    case DISCARD_MESSAGE:
       messagesManager.populateCollection(payload);
       return messagesManager.state();
 
