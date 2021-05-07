56d604144b83985ad3c36d19154ed39dfb6ea2ed
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-06 19:37:33 -0700

    Add docs and run formatter

--- frontend/src/store/reducers/ui-notifications.js
+++ frontend/src/store/reducers/ui-notifications.js
@@ -1,27 +1,42 @@
-import { UINotificationCollection } from "../models/UINotification"
+import { UINotificationCollection } from "../models/UINotification";
 
 const notificationManager = new UINotificationCollection();
 
 const ADD_NOTIFICATION = "uiNotifications ———> ADD_NOTIFICATION";
 const REMOVE_NOTIFICATION = "uiNotifications ———> REMOVE_NOTIFICATION";
 
+/**
+ * Adds a UI notification to the notifications collection and persists
+ * entire collection to localStorage.
+ *
+ * @param {Object} config
+ */
 export const addUINotification = ({ body, type }) => ({
   type: ADD_NOTIFICATION,
   payload: { body, type, created_at: new Date().toISOString() },
 });
 
-export const removeUINotification = ({ id }) => ({ type: REMOVE_NOTIFICATION, payload: { id } })
+/**
+ * Removes a UI notification from the notifications collection and persists
+ * entire collection to localStorage.
+ *
+ * @param {Object} config
+ */
+export const removeUINotification = ({ id }) => ({
+  type: REMOVE_NOTIFICATION,
+  payload: { id },
+});
 
 const reducer = (state = notificationManager.state(), { type, payload }) => {
   switch (type) {
     case ADD_NOTIFICATION:
-      notificationManager.add(payload)
-      notificationManager.syncToLocalStorage()
+      notificationManager.add(payload);
+      notificationManager.syncToLocalStorage();
       return notificationManager.state();
 
     case REMOVE_NOTIFICATION:
-      notificationManager.remove(payload)
-      notificationManager.syncToLocalStorage()
+      notificationManager.remove(payload);
+      notificationManager.syncToLocalStorage();
       return notificationManager.state();
 
     default:
