b10e907fb5fea582b5e27a8e30287ef51f51730b
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-08 17:53:50 -0700

    Refactor & add docs

--- frontend/src/store/reducers/ui-notifications.js
+++ frontend/src/store/reducers/ui-notifications.js
@@ -1,41 +1,63 @@
-import { UINotificationCollection } from "../models/UINotification";
+/** @module store/reducers/ui-notifications */
+
+import { UINotificationCollection } from "../models/Notification";
 
 export const notificationManager = new UINotificationCollection();
 
 const ADD_NOTIFICATION = "uiNotifications ———> ADD_NOTIFICATION";
-const REMOVE_NOTIFICATION = "uiNotifications ———> REMOVE_NOTIFICATION";
+const SEE_NOTIFICATION = "uiNotifications ———> SEE_NOTIFICATION";
+const DISMISS_NOTIFICATION = "uiNotifications ———> DISMISS_NOTIFICATION";
 
 /**
  * Adds a UI notification to the notifications collection and persists
  * entire collection to localStorage.
- *
- * @param {Object} config
+ * @param {String} body
+ * @returns Redux action for reducer
  */
-export const addUINotification = ({ body, type }) => ({
-  type: ADD_NOTIFICATION,
-  payload: { body, type, created_at: new Date().toISOString() },
-});
+export const addUINotification = (body) => {
+  const createdAt = new Date().toISOString();
+
+  const type = "INFO";
+
+  return { type: ADD_NOTIFICATION, payload: { body, type, createdAt } };
+};
 
 /**
- * Removes a UI notification from the notifications collection and persists
+ * Adds datestamp for the moment a notification is seen and then persists
  * entire collection to localStorage.
- *
- * @param {Object} config
+ * @param {String} id
+ * @returns Redux action for reducer
  */
-export const removeUINotification = ({ id }) => ({
-  type: REMOVE_NOTIFICATION,
-  payload: { id },
-});
+export const seeUINotification = (id) => {
+  notificationManager.filter((x) => x.id === id).see();
+
+  return { type: SEE_NOTIFICATION, payload: { id } };
+};
+
+/**
+ * Adds datestamp for the moment a notification is dismissed and then
+ * persists entire collection to localStorage.
+ * @param {String} id
+ * @returns Redux action for reducer
+ */
+export const dismissUINotification = (id) => {
+  notificationManager.filter((x) => x.id === id).dismiss();
+
+  return { type: DISMISS_NOTIFICATION, payload: { id } };
+};
 
 const reducer = (state = notificationManager.copy(), { type, payload }) => {
   switch (type) {
     case ADD_NOTIFICATION:
-      notificationManager.add(payload);
+      notificationManager.produceFrom([payload]);
       notificationManager.syncToLocalStorage();
       return notificationManager.copy();
 
-    case REMOVE_NOTIFICATION:
-      notificationManager.remove(payload);
+    case SEE_NOTIFICATION:
+      notificationManager.syncToLocalStorage();
+      return notificationManager.copy();
+
+    case DISMISS_NOTIFICATION:
       notificationManager.syncToLocalStorage();
       return notificationManager.copy();
 
