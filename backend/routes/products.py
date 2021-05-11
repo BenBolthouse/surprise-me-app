aa7be608e75a76b0fc0c0b91e5041a16f979cf7f
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-11 14:42:24 -0700

    Implement GET route

--- backend/routes/products.py
+++ backend/routes/products.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, jsonify, request
+from sqlalchemy import and_, or_
+
+
+from models import db, Product
 
 
 product_routes = Blueprint("product_routes", __name__, url_prefix="/api/v1/products")
@@ -6,10 +10,27 @@ product_routes = Blueprint("product_routes", __name__, url_prefix="/api/v1/produ
 
 # GET https://surprise-me.benbolt.house/api/v1/products?lon...&lat...&lev...
 # Given a geocoordinate location retrieves a list of all nearby products.
-@product_routes.route("")
+@product_routes.route("", methods=["GET"])
 def get():
-    latitude = request.params.get("lat")
-    longitude = request.params.get("lon")
-    zoom_level = request.params.get("lev")
+    latitude = float(request.args.get("lat"))
+    longitude = float(request.args.get("lon"))
+    radius = float(request.args.get("rad"))
 
-    raise Exception("Not implemented")
+    lat_upper_bound = latitude + radius
+    lat_lower_bound = latitude - radius
+    lon_upper_bound = longitude + radius
+    lon_lower_bound = longitude - radius
+
+    products = Product.query.filter(
+        and_(
+            Product.latitude < lat_upper_bound,
+            Product.latitude > lat_lower_bound,
+            Product.longitude < lon_upper_bound,
+            Product.longitude > lon_lower_bound)).all()
+
+    products = [x.to_dict() for x in products]
+
+    return jsonify({
+        "message": "Success",
+        "data": products,
+    })
