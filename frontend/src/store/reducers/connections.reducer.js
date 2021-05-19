f9b4ac9ff62836349889aed3922cb3c778a887b3
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-19 07:37:21 -0700

    Add get connections to reducer

--- frontend/src/store/reducers/connections.reducer.js
+++ frontend/src/store/reducers/connections.reducer.js
@@ -5,8 +5,29 @@ const model = {
   approved: [],
 };
 
-const reducer = (state = model, { type }) => {
+const reducer = (state = model, { type, payload }) => {
+  const stateCopy = { ...state };
+
   switch (type) {
+    case "connections/GET":
+      stateCopy.requested = payload.connections.filter((x) => {
+        const a = x.requestorId === payload.userId;
+        const b = x.approvedAt === null;
+        return a && b;
+      });
+
+      stateCopy.pending = payload.connections.filter((x) => {
+        const a = x.requestorId !== payload.userId;
+        const b = x.approvedAt === null;
+        return a && b;
+      });
+
+      stateCopy.approved = payload.connections.filter(
+        (x) => x.approvedAt !== null
+      );
+
+      return stateCopy;
+
     default:
       return state;
   }
