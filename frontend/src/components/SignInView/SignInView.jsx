811ba8a5b61be822166f7ccc51b323aa506148fe
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-19 16:08:18 -0700

    Create and style sign in view

--- frontend/src/components/SignInView/SignInView.jsx
+++ frontend/src/components/SignInView/SignInView.jsx
@@ -1,9 +1,138 @@
-import React from "react";
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
+
+import { useDispatch } from "react-redux";
+import { validate } from "validate.js"
+import React, { useEffect, useRef, useState } from "react";
+import {
+  IoLockOpenOutline as Lock,
+  IoMailOpenOutline as Email,
+  IoCloseCircleOutline as Close,
+} from "react-icons/io5"
+import Loader from "react-loaders";
+import 'loaders.css/src/animations/square-spin.scss'
+
+import * as actions from "../../store/actions";
+import TextInput from "../../components/TextInput/TextInput";
+
+import "./sign_in_view.css";
+import { Link } from "react-router-dom";
 
 const SignInView = () => {
+  const dispatch = useDispatch();
+  const formRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  const [fetching, setFetching] = useState(false);
+  const [email, setEmail] = useState({ value: "", errors: [], showErrors: false });
+  const [password, setPassword] = useState({ value: "" });
+
+  const emailOnChange = (evt) => {
+    setEmail({ ...email, value: evt.target.value });
+  }
+
+  const emailOnBlur = () => {
+    const constraints = {
+      email: {
+        presence: {
+          allowEmpty: false,
+          message: "is required.",
+        },
+        format: {
+          pattern: /^(?:(?:[\w`~!#$%^&*\-=+;:{}'|,?/]+(?:(?:\.(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?/.()<>[\] @]|\\"|\\\\)*"|[\w`~!#$%^&*\-=+;:{}'|,?/]+))*\.[\w`~!#$%^&*\-=+;:{}'|,?/]+)?)|(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?/.()<>[\] @]|\\"|\\\\)+"))@(?:[a-zA-Z\d-]+(?:\.[a-zA-Z\d-]+)*\.[a-zA-Z].{1,4})$/,
+          message: "must be a valid email address.",
+        },
+      }
+    }
+
+    const result = validate({ email: email.value }, constraints);
+
+    setEmail({ ...email, errors: result ? result.email : [] });
+  }
+
+  const emailOnErrorClick = () => {
+    setEmail({ ...email, showErrors: !email.showErrors });
+  }
+
+  const passwordOnChange = (evt) => {
+    setPassword({ value: evt.target.value });
+  }
+
+  const formOnSubmit = async (evt) => {
+    evt.preventDefault();
+
+    setFetching(true);
+
+    await dispatch(actions.session.post({
+      email: email.value,
+      password: password.value,
+    }));
+
+    setFetching(false);
+  };
+
+  useEffect(() => {
+    const readyToTry = !fetching
+      && !email.errors.length
+      && email.value.length
+      && password.value.length;
+
+    buttonRef.current.disabled = !readyToTry;
+  }, [fetching, email.errors, email.value, password.value])
+
   return (
-    <h1>SignInView</h1>
-  )
-}
+    <div className="view sign-in-view">
+      {!email.showErrors ? null :
+        <div className="view errors-overlay">
+          <div className="center">
+            <Close className="close" onClick={emailOnErrorClick} />
+            <h2>Please correct the following:</h2>
+            <ul>
+              {email.errors.map((error, i) => (
+                <li key={"sign-in_email-error-" + i}>
+                  <span className="check">✅</span>
+                  {error}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      }
+      {!fetching ? null :
+        <div className="view fetching">
+          <Loader type="square-spin" />
+        </div>
+      }
+      <div className="center">
+        <h1>Sign In</h1>
+        <form ref={formRef} onSubmit={formOnSubmit}>
+          <TextInput
+            icon={<Email />}
+            onChange={emailOnChange}
+            onBlur={emailOnBlur}
+            onErrorClick={emailOnErrorClick}
+            errors={email.errors}
+            name="email"
+            formName="sign-in"
+            label="Email" />
+          <TextInput
+            icon={<Lock />}
+            onChange={passwordOnChange}
+            errors={[]}
+            name="password"
+            formName="sign-in"
+            label="Password"
+            obscure={true} />
+          <button
+            ref={buttonRef}
+            type="submit">
+            Go
+          </button>
+        </form>
+        <Link to="sign-up">Sign up</Link>
+      </div>
+    </div>
+  );
+};
 
 export default SignInView;
