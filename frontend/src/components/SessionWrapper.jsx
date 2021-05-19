a5866b7073a5c35f847b48244c9948a1e876913c
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-19 07:36:00 -0700

    Add docs & fetch actions for authenticated

--- frontend/src/components/SessionWrapper.jsx
+++ frontend/src/components/SessionWrapper.jsx
@@ -1,20 +1,29 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+// eslint-disable-next-line no-unused-vars
+import { ReactElement, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
+import * as actions from "../store/actions";
+
+/**
+ * Component sits below the high order components in the tree to handle
+ * fetching user and user-related data on render. Component also handles
+ * redirection to a sign in page when a session isn't available.
+ * @param {object} props
+ * @return {ReactElement}
+ */
 export const Authenticated = ({ children }) => {
+  const dispatch = useDispatch();
   const session = useSelector(x => x.session);
   const history = useHistory();
 
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    // Immediately redirect if the session doesn't exist...
     if (!session.active) history.push("/sign-in");
-    // Otherwise begin fetching data.
     else {
-      console.log("Starting session context wrapper actions...");
-      // ...
+      dispatch(actions.user.get());
+      dispatch(actions.connections.get());
 
       setMounted(true);
     }
@@ -23,6 +32,13 @@ export const Authenticated = ({ children }) => {
   return !mounted ? null : children;
 };
 
+/**
+ * Component sits below the high order components in the tree and handles
+ * redirection to the app when a session exists, so as to avoid
+ * unauthenticated actions from occurring while the user is authenticated.
+ * @param {object} props
+ * @return {ReactElement}
+ */
 export const Anonymous = ({ children }) => {
   const session = useSelector(x => x.session);
   const history = useHistory();
@@ -30,9 +46,8 @@ export const Anonymous = ({ children }) => {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    // Immediately redirect to the app if the session exists...
     if (session.active) history.push("/");
-    // Otherwise set the components as mounted.
+
     else setMounted(true);
   }, [session.active]);
 
