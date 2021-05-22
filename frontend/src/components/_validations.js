6a79c0e52ed252b7c35a7f11f46cb9919b4869d4
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-22 14:54:26 -0700

    Add validations for passwords

--- frontend/src/components/_validations.js
+++ frontend/src/components/_validations.js
@@ -1,4 +1,4 @@
-import { validate } from "validate.js"
+import { validate } from "validate.js";
 
 export const firstName = (value) => {
   let result = validate({ firstName: value }, constraints.firstName);
@@ -16,6 +16,21 @@ export const email = (value) => {
   let result = validate({ email: value }, constraints.email);
 
   return result ? result.email : [];
+};
+
+export const password = (value) => {
+  let result = validate({ password: value }, constraints.password);
+
+  return result ? result.password : [];
+};
+
+export const confirmPassword = (value, confirmValue) => {
+  let result = validate(
+    { password: value, confirmPassword: confirmValue },
+    constraints.confirmPassword
+  );
+
+  return result ? result.confirmPassword : [];
 };
 
 const constraints = {
@@ -53,6 +68,33 @@ const constraints = {
         pattern:
           /^(?:(?:[\w`~!#$%^&*\-=+;:{}'|,?/]+(?:(?:\.(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?/.()<>[\] @]|\\"|\\\\)*"|[\w`~!#$%^&*\-=+;:{}'|,?/]+))*\.[\w`~!#$%^&*\-=+;:{}'|,?/]+)?)|(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?/.()<>[\] @]|\\"|\\\\)+"))@(?:[a-zA-Z\d-]+(?:\.[a-zA-Z\d-]+)*\.[a-zA-Z].{1,4})$/,
         message: "must be a valid email address.",
+      },
+    },
+  },
+  password: {
+    password: {
+      presence: {
+        allowEmpty: false,
+        message: "is required.",
+      },
+      format: {
+        pattern:
+          /^(?!.* )(?=.*\d)(?=.+\W)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,32}$/,
+        message:
+          "must have at least one uppercase letter, lowercase letter, one special character and cannot contain spaces.",
+      },
+    },
+  },
+  confirmPassword: {
+    confirmPassword: {
+      presence: {
+        allowEmpty: false,
+        message: "is required.",
+      },
+      equality: {
+        attribute: "password",
+        message: "must match password.",
+        comparitor: (a, b) => a === b,
       },
     },
   },
