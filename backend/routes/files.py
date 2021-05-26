1652da081c96bba16d70316c3fd4480784496eb6
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-26 10:56:58 -0700

    Implement POST route

--- backend/routes/files.py
+++ backend/routes/files.py
@@ -1,23 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required
+from werkzeug import secure_filename
+from os import path
 
-from .utils import upload_file, download_file
-
+from .utilities import random_string
 
 file_routes = Blueprint("file_routes", __name__, url_prefix="/file/v1")
 
 
-# POST https://surprise-me.benbolt.house/file/v1
-# Accepts a file object for upload and persists to cloud storage.
-@file_routes.route("", methods=["POST"])
-@login_required
-def post():
-    raise Exception("Not implemented")
+# POST https://surprise-me.benbolt.house/file/v1/t
+# Accepts a file object and persists to temporary storage. Returns the
+# filename for frontend followup downloads.
+@file_routes.route("/t", methods=["POST"])
+def post_temp():
+    file = request.files[0]
+
+    filename = random_string(16)
+
+    file.save(path.join("..", "temp", filename))
+
+    return jsonify({
+        "data": filename,
+    }), 201
 
 
 # POST https://surprise-me.benbolt.house/file/v1
 # Retrieves and sends a file from cloud storage.
-@file_routes.route("", methods=["GET"])
+@file_routes.route("/t", methods=["GET"])
 @login_required
 def get():
     raise Exception("Not implemented")
