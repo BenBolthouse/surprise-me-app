017d16660d0bf0eb591b572bad96e45806cce624
Ben Bolthouse <benbolthouse@gmail.com>
2021-06-07 16:44:40 -0700

    Integrate SessionHandler components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
-import { SignInView, SignUpView, StartView } from "./views";
+import { SignInView, SignUpView } from "./views";
 
 import "./styles/index.css";
+import { Anonymous, Authenticated, SessionHandler } from "./components";
+import { useDispatch } from "react-redux";
+import { actions } from "./store";
 
 const App = () => {
-  return (
-    <Switch>
-      <Route exact path="/start/sign-up">
-        <SignUpView />
-      </Route>
-      <Route path="/start">
-        <SignInView />
-        <StartView />
-      </Route>
-      <Route path="/app">
-      </Route>
-    </Switch>
+  const dispatch = useDispatch();
+
+  const [mounted, setMounted] = useState(false);
+
+  // Side effect awaits retrieval of data and then sets the component state
+  // to mounted.
+  useEffect(() => {
+    async function go() {
+      await dispatch(actions.session.getCsrf());
+    }
+    go();
+
+    setMounted(true);
+  }, [])
+
+  return !mounted ? null : (
+    <SessionHandler>
+      <Switch>
+        <Route exact path="/start/sign-up">
+          <Anonymous redirect="/app">
+            <SignUpView />
+          </Anonymous>
+        </Route>
+        <Route path="/start">
+          <Anonymous redirect="/app">
+            <SignInView />
+            {/* <StartView /> */}
+          </Anonymous>
+        </Route>
+        <Route path="/app">
+          <Authenticated redirect="/start">
+            asdf
+          </Authenticated>
+        </Route>
+      </Switch>
+    </SessionHandler>
   );
 }
 
